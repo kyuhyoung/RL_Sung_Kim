1bdf673fd612412c8ab6ef10a90ad336841f83d2
kevin <kyuhyoung@gmail.com>
2017-02-20 01:18:27 +0900

    Add lab5-1-1 which is stochastice version of lab2
    Due to following error, I had to initialize '_episode_started_at' as 0
     assert self._episode_started_at is not None, "Cannot call env.step() before calling reset()"

---
+++ lab5-1-1.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import gym
+import readchar
+
+
+LEFT, DOWN, RIGHT, UP = 0, 1, 2, 3
+
+arrow_keys = {
+    '\x1b[A' : UP,
+    '\x1b[B': DOWN,
+    '\x1b[C': RIGHT,
+    '\x1b[D': LEFT }
+
+env = gym.make("FrozenLake-v0")
+env._episode_started_at = 0
+env.render()
+
+
+while True:
+    #key = inkey()
+    key = readchar.readkey()
+    if key not in arrow_keys.keys():
+        print('Game aborted !!')
+        break
+    action = arrow_keys[key]
+    #print('action key input : ' + str(action))
+    #print('env._episode_started_at : ' + str(env._episode_started_at))
+    state, reward, done, info = env.step(action)
+    env.render()
+    print('State : ', state, "Action : ", action, "Reward : ", reward, "Info : ", info)
+    if done:
+        print("Finished with reward", reward)
+        break
